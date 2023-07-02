7c36a230465448edb0a9ad1fb5a3a6da63f20bf1
Kinan BSHARA <kbshara@myges.fr>
2023-07-02 12:58:32 +0200

    restructuring as mono service

--- auth-service/prisma/RefreshToken.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod"
-
-export const RefreshTokenSchema = z.object({
-    refreshToken: z.string(),
-    createdAt: z.date(),
-    userId: z.string(),
-    deviceIp: z.string().ip(),
-    blacklisted: z.boolean().default(false)
-})
--- temporal-service/dist/src/timeboxs/dto/update-timebox.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateTimeboxDto } from './create-timebox.dto';
-declare const UpdateTimeboxDto_base: import("@nestjs/common").Type<Omit<CreateTimeboxDto, "createdBy">>;
-export declare class UpdateTimeboxDto extends UpdateTimeboxDto_base {
-}
-export {};
